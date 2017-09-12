47ca012227ceb9c732359e60d91a34168f08788a
John Norwood <norwood.john.m@gmail.com>
2017-09-12 00:42:29 -0500

    Changed versioning strategy in the scripts

--- jscripts/settings/application-settings.sh
+++ jscripts/settings/application-settings.sh
@@ -2,8 +2,6 @@
 
 [[ -n "${_APPLICATION_SETTINGS_SH:+_}" ]] && return || readonly _APPLICATION_SETTINGS_SH=1
 
-source ${SCRIPT_DIR}/settings/docker-settings.sh
-
 ##
 # This is where you should define the names of every application that is built by this project, an application being some
 # collection of docker containers that should run as an atomic unit. These names will be used in scripts throughout this
@@ -35,6 +33,12 @@ function get_images_necessary_to_run_app {
     jq -r ".imagesToRunApp.${app}[]" <<< "${_APPLICATION_CONFIG}"
 }
 
+function _get_maven_version {
+    grep -A2 '<groupId>com.johnmalcolmnorwood.hashbash</groupId>' pom.xml \
+        | grep 'version' \
+        | sed 's|.*<version>\(.*\)</version>|\1|'
+}
+
 function get_current_application_version {
-    get_image_version "any"
+    _get_maven_version
 }
